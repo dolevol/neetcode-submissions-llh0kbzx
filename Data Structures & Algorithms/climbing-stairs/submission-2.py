bf9a43c5990a4a22c84506bf49595cb0c1b4317a
dolevol <95031778+dolevol@users.noreply.github.com>
2026-04-01 14:10:41 +0300

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        vals = [-1] * (n)
+
+        # if(n > 1):
+        #         vals[0] = 1
+
+        # if(n > 2):
+        #         vals[1] =  2       
+
+        def climbStairsRec(n: int) -> int:
+                if(n <= 2):
+                        return n        
+
+                if(vals[n-1] == -1):        
+                        vals[n-1] = climbStairsRec(n-1) + climbStairsRec(n-2)
+                
+                return vals[n-1]                                   
+        
+        return climbStairsRec(n)
